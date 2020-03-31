503b8839ab9092bba09f94f37023dd5e03e6f203
cxf <862977812@qq.com>
2020-03-31 09:48:38 +0800

    修改

--- src/main/java/com/foolish/gitdemo/controller/TestController.java
+++ src/main/java/com/foolish/gitdemo/controller/TestController.java
@@ -11,8 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @GetMapping("/test")
+    @GetMapping("/test2")
     public String test(){
-        return "test";
+        return "test2";
     }
 }
