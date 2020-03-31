99842921b225a000466abee51926f7ba05ab7363
cxf <862977812@qq.com>
2020-03-31 11:19:57 +0800

    test

--- src/main/java/com/foolish/gitdemo/controller/TestController.java
+++ src/main/java/com/foolish/gitdemo/controller/TestController.java
@@ -11,8 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @GetMapping("/test2")
+    @GetMapping("/test")
     public String test(){
-        return "test3";
+        return "test";
     }
 }
