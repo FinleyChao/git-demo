ec51dcc876ba565b5efd36b667105fd184aa1a41
cxf <862977812@qq.com>
2020-03-31 09:29:15 +0800

    新建项目

---
+++ src/main/java/com/foolish/gitdemo/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.foolish.gitdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description:
+ * @author：chaoxianfei
+ * @date：2020/3/31 9:26
+ */
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test(){
+        return "test";
+    }
+}
